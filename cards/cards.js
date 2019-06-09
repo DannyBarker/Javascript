28334d6cc98fa07c315b299dcdcf0634458a62ee
Danny Barker <barkerdanny17@yahoo.com>
2019-06-09 11:28:24 -0500

    finished dynamic cards

--- cards/cards.js
+++ cards/cards.js
@@ -1,21 +1,37 @@
 let textArea = document.querySelector("#words");
 let button = document.querySelector("#create");
 let newElement = document.querySelector("#newCard");
-let cardId = 0;
-let delCard = 0;
-button.addEventListener('click', () => {
+let cardNum = 0;
+const createCard = () => {
     let userInput = textArea.value
-    cardId++;
-    delCard++;
+    cardNum++;
     newElement.innerHTML += `
-    <article class="card" id="card--${cardId}">
+    <article class="card" id="card--${cardNum}">
     <div>${userInput}</div>
     <div>
-        <button id="delete--${delCard}">Delete This Card</button>
+    <button class= "delete" id="delete--${cardNum}">Delete This Card</button>
     </div>
     </article>
     `
     textArea.value = "";
-    
-})
 
+    addDeleteEvent()
+       
+}
+
+const delButton = () => {
+    let cardId = event.target.id.split('--');
+    const deleteNum = `card--${cardId[1]}`;
+    const deleteThis = document.getElementById(deleteNum);
+    deleteThis.parentNode.removeChild(deleteThis);
+    
+}
+
+const addDeleteEvent = () => {
+    let allDelButtons = document.querySelectorAll(".delete");
+    allDelButtons.forEach(byeButton => {
+        byeButton.addEventListener('click', delButton)
+    });
+}
+
+button.addEventListener('click', createCard)
